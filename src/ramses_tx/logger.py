60efa2095e37f273b5d216eeb10c5ead086785f5
Egbert Broerse <dcc2@ebroerse.nl>
2025-09-17 16:27:03 +0200

    lint imports

--- src/ramses_tx/logger.py
+++ src/ramses_tx/logger.py
@@ -7,7 +7,6 @@ This module wraps logger to provide bespoke functionality, especially for timest
 from __future__ import annotations
 
 import logging
-# import os
 import re
 import shutil
 import sys
