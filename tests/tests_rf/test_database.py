9145adf4023b3b3291a7c4a141b6f527bd92cbae
Egbert Broerse <dcc2@ebroerse.nl>
2025-10-06 18:00:26 +0200

    extend db test

--- tests/tests_rf/test_database.py
+++ tests/tests_rf/test_database.py
@@ -1,23 +1,44 @@
 #!/usr/bin/env python3
 """RAMSES RF - Unit test for MessageIndex."""
 
-from datetime import datetime as dt
+import contextlib
+from datetime import datetime as dt, timedelta as td
 
 from ramses_rf.database import MessageIndex
 from ramses_tx import Message, Packet
 
+_SRC1 = "32:166025"
+_SRC2 = "01:087939"  # (CTR)
+_NOW = dt.now().replace(microsecond=0)
+pkt1 = Packet(_NOW, "...  I --- 32:166025 --:------ 32:166025 1298 003 007FFF")
+msg1: Message = Message._from_pkt(pkt1)
+pkt2 = Packet(
+    _NOW + td(seconds=10), "...  I --- 32:166025 --:------ 32:166025 1298 003 001230"
+)
+msg2: Message = Message._from_pkt(pkt2)
+pkt3 = Packet(
+    _NOW + td(seconds=20),
+    "060  I --- 01:087939 --:------ 01:087939 2309 021 0007D00106400201F40301F40401F40501F40601F4",
+)
+msg3: Message = Message._from_pkt(pkt3)
+pkt4 = Packet(
+    _NOW + td(seconds=30),
+    "060  I --- 32:166025 --:------ 32:166025 31DA 030 00EF00019E00EF06E17FFF08020766BE09001F0000000000008500850000",
+)
+msg4: Message = Message._from_pkt(pkt4)
+
 
 async def test_add_msg() -> None:
     """Add a message to the MessageIndex."""
-
     msg_db = MessageIndex()
+    ret: Message | None = None
 
-    pkt1 = Packet(dt.now(), "...  I --- 32:166025 --:------ 32:166025 1298 003 007FFF")
-    msg1: Message = Message._from_pkt(pkt1)
+    # add a message
+    assert msg1.payload == {
+        "co2_level": None,
+    }, "unexpected parsed payload"
 
-    if msg_db:  # central SQLite MessageIndex
-        ret = msg_db.add(msg1)
-        # replaced message that might be returned
+    ret = msg_db.add(msg1)  # replaced message
 
     assert ret is None
     assert msg_db.contains(code="1298")
@@ -26,16 +47,86 @@ async def test_add_msg() -> None:
         str(msg_db.all()) == "( I --- 32:166025 --:------ 32:166025 1298 003 007FFF,)"
     )
 
-    pkt2 = Packet(dt.now(), "...  I --- 32:166025 --:------ 32:166025 1298 003 007FFF")
-    msg2: Message = Message._from_pkt(pkt2)
+    # add another message with same code
+    ret = msg_db.add(msg2)  # replaced message
 
-    if msg_db:  # central SQLite MessageIndex
-        ret = msg_db.add(msg2)
-        # replaced message that might be returned
     assert (
         str(ret)
         == "||  32:166025 |            |  I | co2_level        |      || {'co2_level': None}"
     )
+    assert len(msg_db.all()) == 1
 
+    # add another message with different code
+    ret = msg_db.add(msg3)  # replaced message
+
+    assert ret is None
+    assert len(msg_db.all()) == 2
+
+    # test clear index
     msg_db.clr()
     assert len(msg_db.all()) == 0
+
+
+async def test_qry_msg() -> None:
+    """Query the MessageIndex."""
+    msg_db = MessageIndex()
+    msg_db.add(msg1)
+    msg_db.add(msg2)
+    msg_db.add(msg3)
+    msg_db.add(msg4)
+
+    # qry by code
+    assert msg_db.contains(code="2309"), "code missing"
+    assert msg_db.contains(src="01:087939", code="2309"), "src missing"
+    assert not msg_db.contains(src="01:12345", code="2309"), (
+        "random src should return False"
+    )
+    assert not msg_db.contains(code="1234"), "a random code should return False"
+    assert msg_db.contains(dst="01:087939"), "dst missing"
+    assert not msg_db.contains(plk="co2_level"), "payload keys skipped if value is None"
+
+    with contextlib.suppress(ValueError):
+        msg_db.qry_field("RANDOM from messages", (_SRC1, _SRC1))
+    # Only SELECT queries are allowed
+
+    # Use simplest SQLite query on MessageIndex
+    sql = """
+            SELECT code, plk from messages WHERE (src = ? OR dst = ?)
+        """
+    res: list[tuple[dt, str]] = msg_db.qry_field(sql, (_SRC2, _SRC2))
+    assert res == [
+        (
+            "2309",
+            "|zone_idx|setpoint|",
+        ),
+    ]
+
+    # Use multi-field SQLite query on MessageIndex
+    sql = """
+            SELECT code, plk from messages WHERE verb in (' I', 'RP')
+            AND (src = ? OR dst = ?)
+            AND code in ('1298', '31DA')
+            AND (plk LIKE '%co2_level%')
+        """
+    res: list[tuple[dt, str]] = msg_db.qry_field(sql, (_SRC1, _SRC1))
+    assert res == [  # key 'co2_level' included since value is not None
+        ("1298", "|co2_level|"),
+        (
+            "31DA",
+            "|hvac_id|exhaust_fan_speed|fan_info|_unknown_fan_info_flags|co2_level|indoor_humidity|exhaust_temp|indoor_temp|outdoor_temp|speed_capabilities|bypass_position|supply_fan_speed|remaining_mins|post_heat|pre_heat|supply_flow_fault|exhaust_flow_fault|_extra|",
+        ),
+    ]
+    assert msg_db.contains(plk="|co2_level|"), "payload keys missing"
+
+    # Use payload key SQLite query on MessageIndex
+    sql = """
+            SELECT code, ctx from messages WHERE verb in (' I', 'RP')
+            AND (src = ? OR dst = ?)
+            AND (plk LIKE '%co2_level%')
+        """
+    res: list[tuple[dt, str]] = msg_db.qry_field(sql, (_SRC1, _SRC1))
+    assert res == [("1298", "False"), ("31DA", "00")]
+
+    assert msg_db.contains(plk="|co2_level|"), "payload keys missing"
+
+    assert len(msg_db.all()) == 3
