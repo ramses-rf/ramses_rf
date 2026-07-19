f27a0a7c13ccfd8d89c53394519b955912c3a99e
PWhite-Eng <75939018+PWhite-Eng@users.noreply.github.com>
2026-07-19 21:07:04 +0100

    Refactor: Implement Round 4C-II System Domain Legacy Purge

--- src/ramses_tx/command/__init__.py
+++ src/ramses_tx/command/__init__.py
@@ -3,7 +3,8 @@
 
 from __future__ import annotations
 
-from ..const import I_, RP, RQ, W_, Code
+from collections.abc import Callable
+
 from .base import CommandBase
 from .system import SystemMixins
 
@@ -16,20 +17,4 @@ class Command(SystemMixins, CommandBase):
 
 
 # A convenience dict
-CODE_API_MAP = {
-    f"{RP}|{Code._3EF1}": Command.put_actuator_cycle,
-    f"{I_}|{Code._3EF0}": Command.put_actuator_state,
-    f"{RQ}|{Code._1030}": Command.get_mix_valve_params,
-    f"{W_}|{Code._1030}": Command.set_mix_valve_params,
-    f"{RQ}|{Code._3220}": Command.get_opentherm_data,
-    f"{RQ}|{Code._0404}": Command.get_schedule_fragment,
-    f"{W_}|{Code._0404}": Command.set_schedule_fragment,
-    f"{RQ}|{Code._0006}": Command.get_schedule_version,
-    f"{RQ}|{Code._0418}": Command.get_system_log_entry,
-    f"{W_}|{Code._2E04}": Command.set_system_mode,
-    f"{RQ}|{Code._313F}": Command.get_system_time,
-    f"{W_}|{Code._313F}": Command.set_system_time,
-    f"{RQ}|{Code._1100}": Command.get_tpi_params,
-    f"{W_}|{Code._1100}": Command.set_tpi_params,
-    f"{I_}|{Code._0002}": Command.put_weather_temp,
-}
+CODE_API_MAP: dict[str, Callable[..., Command]] = {}
